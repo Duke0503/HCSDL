6cdc9576704c3598b2810317698c2ca5dd0e68b9
MinhHieu212 <hieu.tranminhk21@hcmut.edu.vn>
2023-12-14 21:31:44 +0700

    save code add cors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 
 const moment = require("moment");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const database = require("./config/database");
 
@@ -18,6 +18,19 @@ database.connect();
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4000");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
