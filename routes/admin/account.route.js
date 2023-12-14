bcd7ff447c3b956070e84e18ac50c288b7a1dd8c
Duke0503 <minhducgl0503@gmail.com>
2023-12-14 09:41:03 +0700

    Update

---
+++ routes/admin/account.route.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const controller = require("../../controllers/admin/account.controller");
+
+router.get("/", controller.index);
+
+router.get("/search/:name", controller.searchAccount);
+
+router.get("/search/type/:ty", controller.searchType);
+
+router.get("/search/:name/:ty", controller.search);
+
+module.exports = router;
